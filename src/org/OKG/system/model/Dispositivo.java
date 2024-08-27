2ab94b4b19e1863a012506fd33f227a8582779e6
Randy <roscal-202379@kinal.edu.gt>
2024-08-27 08:15:31 -0600

    feat($model): se agrego el modelo dispositivo

---
+++ src/org/OKG/system/model/Dispositivo.java
@@ -0,0 +1,45 @@
+package org.OKG.system.model;
+
+public class Dispositivo {
+    
+    public Dispositivo(){
+
+    }
+
+    private String marca;
+    private String modelo;
+    private int año;
+
+    public Dispositivo(String marca, String modelo, int año) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
+
+}
