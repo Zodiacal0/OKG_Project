4e31f0999d0606780fb8b04b5333db9581d16de9
Randy <roscal-202379@kinal.edu.gt>
2024-08-27 08:30:29 -0600

    feat($model): implementacion del modelo Telefono

---
+++ src/org/OKG/system/model/Telefono.java
@@ -0,0 +1,52 @@
+package org.OKG.system.model;
+
+public class Telefono extends Dispositivo {
+    
+    private String marca = "SamSung";
+    private String modelo = "S24 Ultra";
+    private int año = 2024;
+    private String bateria = "5000mH";
+
+    public Telefono() {
+    }
+
+    public Telefono(String marca, String modelo, int año) {
+        super(marca, modelo, año);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getBateria() {
+        return bateria;
+    }
+
+    public void setBateria(String bateria) {
+        this.bateria = bateria;
+    }
+
+    
+    
+
+}
